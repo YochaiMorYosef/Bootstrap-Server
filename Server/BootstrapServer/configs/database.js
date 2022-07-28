9cb641b1f08db84563bd642c43f719fb62b82bab
YochaiMorYosef <104884523+YochaiMorYosef@users.noreply.github.com>
2022-07-28 18:17:09 +0300

    Update database.js

--- Server/BootstrapServer/configs/database.js
+++ Server/BootstrapServer/configs/database.js
@@ -1,35 +1,3 @@
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost:27017/bootstrapserverDB');
-/*
-// Requires !!!
-var mongodb = require("mongodb");
-
-var client = mongodb.MongoClient;
-var url = "mongodb://localhost:27017/bootstrapserverDB";
-client.connect(url, function(err, client) {
-
-    var db = client.db("bootstrapserverDB");
-    var collection = db.collection("requests");
-
-    var query = {};
-
-    var cursor = collection.find(query);
-
-    cursor.forEach(
-        function(doc) {
-            console.log(doc);
-        },
-        function(err) {
-            client.close();
-        }
-    );
-
-    db.collection("requests").createIndex({ "Client_pass": 1 }, { expireAfterSeconds: 10 },
-        (err, dbResult) => {
-            if (err) throw err;
-            console.log("Index Created");
-        });
-
-
-});*/
